fef3a713b3975d5484e7f14d90344d552e463f8a
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-09-12 16:57:26 +0300

    Spring Boot added

---
+++ src/main/java/com/workintech/SecurityBasics/SecurityBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.SecurityBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBasicsApplication.class, args);
+	}
+
+}
